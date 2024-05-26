d51ff5cef568b545723c1a36d2a9360c5c2dd932
Petia Mladenova <135381775+Petia88@users.noreply.github.com>
2024-05-26 21:26:52 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
-function chainedPromises() {
-   console.log("Start");
-   setTimeout(function(){
-    console.log("1");
-    setTimeout(function(){
-        console.log("2");
-        setTimeout(function(){
-            console.log("3")
-        }, 1000)
-    }, 1000)
-   }, 1000)
-}
+function simplePromise() {
+    let promise = new Promise(function(resolve, reject){
+     setTimeout(function(){
+        resolve("Success");
 
-//with promise
-function wait(ms)
-{
-    return new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve();
-    }, ms)
+     }, 2000)
     })
-}
 
-function chainedPromisesWithPromise()
-{
-    console.log("Start");
-
-    wait(1000)
-    .then(function(){
-        console.log("1");
-        return wait(1000);
-    })
-    .then(function(){
-        console.log("2");
-        return wait(1000);
-    })
-    .then(function(){
-        console.log("3");
+    promise.then(function(result){
+        console.log(result)
     })
 }
