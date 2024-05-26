ec32ee38eb96f2db1ffb8788584e76ce454810e0
Petia Mladenova <135381775+Petia88@users.noreply.github.com>
2024-05-26 21:30:17 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
-async function chainedPromisesAsync() {
-    let promise1 = new Promise(function(resolve, reject){
-        setTimeout(function(){
-            resolve("Promise 1")
-        }, 1000);
-    })
+async function startQuiz() {
+    
+    let finalScore = 0;
+    for(let index = 0; index < questions.length; index++){
+        const {question, answers, correct} = questions[index];
+        const userInput = await askQuestion(question, answers);
+        if(userInput == correct)
+            {
+                finalScore += 1;
+                console.log("Correct!");
+            }
+            else
+            {
+                console.log("Not correct!");
+            }
+    }
 
-    let promise2 = new Promise(function(resolve, reject){
-        setTimeout(function(){
-            resolve("Promise 2")
-        }, 2000);
-    })
-
-    let promise3 = new Promise(function(resolve, reject){
-        setTimeout(function(){
-            resolve("Promise 3")
-        }, 3000);
-    })
-
-    let result1 = await promise1;
-    let result2 = await promise2;
-    let result3 = await promise3;
-
-    console.log(result1, result2, result3)
+    console.log(`Final result: ` + finalScore);
 }
+
+function askQuestion(question,answers)
+{
+    return new Promise(function(resolve, reject){
+        let message = question + '\n';
+        answers.forEach((answer, index) => message += `${index}. ${answer}\n`)
+            const userInput = prompt(message);
+            resolve(parseInt(userInput));
+        });
+    
+}
+
+const questions= [
+    {
+      question: "What is 2 + 2?",
+      answers: ["3", "4", "5"],
+      correct: 1
+    },
+    {
+        question: "What is the capital of France?",
+        answers: ["Berlin", "Madrid", "Paris"],
+        correct: 2
+      },
+      {
+        question: "What is the square root of 16?",
+        answers: ["4", "5", "6"],
+        correct: 0
+      }
+]
